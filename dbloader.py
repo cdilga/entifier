f38574c1a48b6b8c10cb581a26afec5c5cd02344
cdilga <c.dilger1@gmail.com>
2018-11-19 18:30:49 -0500

    Now implements successful classifier distance metric with cutoff cosine

--- dbloader.py
+++ dbloader.py
@@ -13,6 +13,15 @@ import urllib
 from neo4j.v1 import GraphDatabase
 import string
 
+import nltk
+#nltk.download('stopwords')
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+stop_words = set(stopwords.words('english'))
+import pickle
+from sklearn.neighbors import KNeighborsClassifier
+import sklearn.metrics
+
 class EntityLoader():
     '''
     This object should be able to load a file of entity relations
@@ -72,31 +81,37 @@ def yagoReader(filename):
     with open(filename, encoding="utf8") as f:
         for line in f:
             arr.append([el.replace('<', '').replace('>', '').lower() for el in line.strip().split('\t')])
-    return pd.DataFrame(arr)
+    return pd.DataFrame(arr, columns=['e1', 'rel', 'e2'])
 
 def csvRead(filename):
     #These functions for reading could use improvement
     df = pd.read_csv(filename, sep='\t', lineterminator='\n',
-                     encoding='utf8',dtype=str, error_bad_lines=False).iloc[:, [15, 16, 17]]
-    
+                     encoding='utf8',dtype=str, error_bad_lines=False, header=None).iloc[:, [15, 16, 17]]
+    df = df.rename(columns={15: "e1",
+                       16: "rel", 
+                       17: "e2"})
     translation = {' ': '_'}
     for char in string.punctuation:
         translation[char] = None
     
     for s in df.columns:
-        df[s] = df[s].apply(lambda x: str(x).strip().translate(str.maketrans(translation)))
+        df[s] = df[s].apply(lambda x: ' '.join([word for word in word_tokenize(
+            str(x)) if word not in stop_words]).translate(str.maketrans(translation)).strip())
         
     
     print('Successfully read in file')
     return df
 
 
-#yago = EntityLoader('data/yagoFacts.tsv', yagoReader)
+yago = EntityLoader('data/yagoFacts.tsv', yagoReader)
 #yago.cache()
 
-wiki = EntityLoader('data/wikipedia-partial-output.txt', csvRead)
-wiki.cache()
-wiki.push()
+wiki = EntityLoader('data/wikipedia-full-reverb.txt', csvRead)
+#df = csvRead('data/wikipedia-partial-output.txt')
+print(wiki._df.head())
+#wiki.cache()
+#wiki.push()
+
 def compare(entity1, entity2):
     '''Take two dataframes with 3 columns and compare entities'''
     temp1 = pd.concat([entity1.iloc[:,0], entity1.iloc[:,2]])
@@ -104,5 +119,108 @@ def compare(entity1, entity2):
     result = temp1.isin(temp2)
     return result
 
-#print(compare(wiki._df, yago._df).value_counts())
+def findMatches(entity1, entity2):
 
+    #indicies = 
+    merged = pd.merge(entity1, entity2, left_on=['e1', 'e2'], right_on=['e1', 'e2'])
+    merge2 = pd.merge(entity1, entity2, left_on=[
+                      'e1', 'e2'], right_on=['e2', 'e1'])
+    #merged = entity1.values[entity1.iloc[:, 0].isin(entity2.iloc[:, 0])]
+    return pd.concat([merged, merge2])
+
+#print(compare(wiki._df, yago._df).value_counts())
+#findMatches(wiki._df, yago._df).to_csv(open('matches.csv', 'w'))
+
+def loadGensim():
+    try:
+        gensimmodel = pickle.load(open('gensim.cache', 'rb'))
+        print('Loaded gensim from cache')
+    except: 
+        from gensim.models import KeyedVectors
+        print('Fallback to loading gensim from source')
+        gensimmodel = KeyedVectors.load_word2vec_format(
+            './google-news/GoogleNews-vectors-negative300.bin.gz', binary=True)
+        pickle.dump(gensimmodel, open('gensim.cache', 'wb'))
+        print('Loaded Gensim')
+    
+    return gensimmodel
+class KillClassifier():
+    def __init__(self, threshold):
+        self._threshold =threshold
+
+    def fit(self, x, y):
+        #for the first element 
+        x = np.array(x)
+        y = np.array(y)
+        self.train_x = x
+        self.train_y = y
+
+        
+    def predict(self, x):
+        x = np.array(x)
+        for i, k in enumerate(x):
+            ret = np.empty_like(x)
+            #temp = np.empty_like(x)
+            temp = sklearn.metrics.pairwise.cosine_similarity(np.broadcast_to(x[i], self.train_x.shape), self.train_x)
+            index = np.argmin(temp)
+            if temp[index] > self._threshold:
+                ret[i] = self.train_y[index]
+            else:
+                ret[i] = None
+        return ret
+
+def lookup(gensim, k):
+    try:
+        return gensim[k]
+    except:
+        return np.zeros_like(gensim['the'])
+def convertToEmbeddings(words, gensim):
+    ret = []
+    for i, val in enumerate(words):
+        ret.append(
+            np.mean(np.array([lookup(gensim, k) for k in str(val).split('_')]), axis=0))
+    return np.array(ret)
+def classifyEntities(wiki, yago):
+    '''Maps the wiki classifications to yago ones, or declares them 'out of class' '''
+    gensim = loadGensim()
+    #will use the yago vecs inside a knn that is essentially pretrained....
+    #essentially an argmin of the distances to known classes
+    #make the word vecs of the unique yago classes the training x and the actual words the labels
+    clr = KillClassifier(0.2)
+    
+    #fakeGensim = {'the': np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])}
+
+    training_y = yago.iloc[:, 0].append(yago.iloc[:, 2])
+    
+    training_x = convertToEmbeddings(training_y, gensim)
+    print('Finished converting embeddings')
+    #then find the closest by some distance metric. Then go for it.
+    #print(training_x)
+    #print(training_x)
+
+    clr.fit(training_x, training_y)
+    print('Finished training model')
+    #first convert wiki to word2vec embeddings 
+    #run first column through predict
+
+    #run second column through predict
+    finalDf = pd.DataFrame()
+    e1Embeddings = convertToEmbeddings(wiki['e1'], gensim)
+    print('e1 Embeddings calculated')
+    finalDf['e1'] = clr.predict(e1Embeddings)
+    print('predicted e1')
+    finalDf['rel'] = wiki['rel']
+    e2Embeddings = convertToEmbeddings(wiki['e2'], gensim)
+    print('e2 Embeddings calculated')
+    finalDf['e2'] = clr.predict(e2Embeddings)
+    print('predicted e2')
+
+    
+    return finalDf
+
+    #if it's further than some distance then we just leave it out of class
+
+
+deambiguated = classifyEntities(wiki._df.iloc[:100, :], yago._df)
+
+print(findMatches(deambiguated, yago._df))
